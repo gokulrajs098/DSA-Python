9b6b280a6a49b30909e5c805fe0b3d2d62b526d4
Gokul Raj <serajgokul19@gmail.com>
2024-12-17 21:42:11 +0530

    heapq

---
+++ Queue/Priority_Queue.py
@@ -0,0 +1,14 @@
+import heapq
+
+heap = []
+
+
+heapq.heappush(heap, 20)
+heapq.heappush(heap, 15)
+heapq.heappush(heap, 30)
+heapq.heappush(heap, 25)
+heapq.heappush(heap, 45)
+heapq.heappush(heap, 10)
+heapq.heappush(heap, 16)
+
+print(heap)
