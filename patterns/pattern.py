67142a9e8eba7e718e1d2527e43e1894a575d81c
Gokul Raj <serajgokul19@gmail.com>
2024-12-30 16:56:16 +0530

    patterns *

---
+++ patterns/pattern.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    for j in range(5):
+        print("*", end="")
+    print()
+
+print("----------------------------------------")
+
+for i in range(5):
+    for j in range(i+1):
+        print("*", end="")
+    print()
